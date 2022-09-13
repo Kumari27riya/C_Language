35b193c9c661962bc2d2e8e4c516dcaa27f29a34
Kumari27riya <113409140+Kumari27riya@users.noreply.github.com>
2022-09-13 09:45:30 +0530

    Add files via upload

---
+++ 2Second.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int l,a;
+    printf("Length of square:");
+    scanf("%d",&l);
+    //l= 3;
+    a= l*l;
+    printf("Area of square: %d",a);
+    return 0;
+
+}
